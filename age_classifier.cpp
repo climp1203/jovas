67e03e28fca812b7f4788f4cca9f81363af57505
climp1203 <jovasotieno8@gmail.com>
2025-10-07 11:53:01 +0300

    Create age_classifier.cpp

---
+++ age_classifier.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int age:
+cout<< "Enter your age:";
+cin>> age;
+if(age>= 18)
+cout<< "You are an adult."<<endl;
+else
+cout<< " You are underage."<<endl;
+return 0;
+}
